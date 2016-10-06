af0f3f135296c5f644e674b7712bcca5db5c7bdc
aimee1293 <isc.aimee.paramo@gmail.com>
2016-10-05 22:22:50 -0700

    Delete ex12.py

--- ex12.py
+++
@@ -1,8 +0,0 @@
-# Paramo Mora Aimee 12210347
-
-age = raw_input("How old are you? ")
-height = raw_input("How tall are you? ")
-weight = raw_input("How much do you weigh? ")
-
-print "So, you're %r old, %r tall and %r heavy." % (
-    age, height, weight)
