8cd11ba99d9b2813ddff66a8e61819ac28892b0d
DavidAngelino <colorianodavid@gmail.com>
2016-10-11 16:48:23 -0700

    Practicas Python

---
+++ Coloriano-Python/48.py
@@ -0,0 +1,7 @@
+Python 2.7.11 (default, May 25 2016, 05:27:56) 
+[GCC 4.2.1 Compatible Apple LLVM 7.3.0 (clang-703.0.29)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> int("hell")
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+ValueError: invalid literal for int() with base 10: 'hell'
