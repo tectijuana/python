8f552d103cbd3838baab05cd6e05990bd5a1269c
jhony <alvaradogodinez@gmail.com>
2016-10-12 17:39:54 -0700

    mis trabajos de python

---
+++ jhonatanpython/programa44.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+class Parent(object):
+
+    def implicit(self):
+        print "PARENT implicit()"
+
+class Child(Parent):
+    pass
+
+dad = Parent()
+son = Child()
+
+dad.implicit()
+son.implicit()
