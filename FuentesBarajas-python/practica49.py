c8b30761c094ec1d52c1d07b78395b8c201d5b3b
Damarcusfuentes <marcos.fuentes02@gmail.com>
2016-10-13 14:13:35 -0700

    Mis programas de python

---
+++ FuentesBarajas-python/practica49.py
@@ -0,0 +1,10 @@
+def parse_subject(word_list):
+    skip(word_list, 'stop')
+    next_word = peek(word_list)
+
+    if next_word == 'noun':
+        return match(word_list, 'noun')
+    elif next_word == 'verb':
+        return ('noun', 'player')
+    else:
+        raise ParserError("Expected a verb next.")
